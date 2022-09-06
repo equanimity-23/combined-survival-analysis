23f2c5af3ff852ff16dd483d323c6aa4f1b95cc7
equanimity-23 <danielchewie@gmail.com>
2022-09-06 16:50:27 +1000

    updated script for coxme, should be working up to that point, but cox.zph and survival curves are still WIP

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # combined-survival-analysis
 Automated combined survival analysis in R
+
+## Daily Notes
+### 6th Sept
+- The code is working up to the coxme analysis of the entire DCV dataset.
+- cox.zph is not working as it requires the coxme object, but I do not know how to pull it into the loop.
+- Other issues I have noticed:
+  - When running coxme as a loop, the data variable is termed as "x", rather than the miRNA itself. I believe this is the nature      of the loop. 
+  - I need to rename all of the datasets to be "DCV_stockNo." as the numerals first is causing issues with the R code. 
--- script/script-for-combined-analysis.R
+++ script/script-for-combined-analysis.R
@@ -1,26 +1,70 @@
 library(survival)
+library(dplyr)
+library(coxme)
+library(survminer)
 
-# Actual data-----------------    
-## Importing data into a list 
-my_files <- list.files(pattern = "*.csv")
-my_data <- lapply(my_files, read.csv)
-names(my_data) <- gsub("*.csv", "", my_files)
+# DCV data----------------- 
+## Import data  
+### list file names -> import using a for loop -> rename files within the data list
+dcv_files <- list.files(path = "DCV_data", pattern = "*.csv")
+
+dcv_data <- list()
+for (i in 1:length(dcv_files)) {
+  dcv_data[[i]] <- read.csv(paste0("DCV_data/", dcv_files[i]), header = T, sep = ",")
+}
+
+names(dcv_data) <- gsub("*.csv", "", dcv_files)
+
+## making sure data is coded as factors
+### not working yet
+
+## removing PBS samples
+dcv_noPBS <- lapply(dcv_data, function(x) filter(x, Virus == 1))
 
 ## Specify the model attributes
-model <- Surv(Days, Survival) ~ Mutant
-
 mixed_effects <- Surv(Days, Survival) ~ Mutant + (1|BiolRep/TechRep)
 
+model <- Surv(Days, Survival) ~ Mutant
+
 ## Run the analysis across the dataset -> pick which one you want
-lapply(my_data, function(x) coxph(model, x))
+dcv_fit <- lapply(dcv_noPBS, function(x) coxme(mixed_effects, x))
 
+lapply(dcv_noPBS, function(x) coxph(model, x)) # CoxPH model
 
-fit = lapply(my_data, function(x) coxme(mixed_effects, x))
+## Split the coxme fit into separate dataframes
+list2env(dcv_fit, .GlobalEnv)
+
 
 
 ## test for cox assumptions
-lapply(my_data, function(x) cox.zph(coxme(mixed_effects, x))) # THIS DOES NOT WORK!
+lapply(dcv_fit, function(x) cox.zph(x)) # THIS DOES NOT WORK!
 # should i split the data list back into coxme objects then loop the cox.zph function through each?
 
 ## plotting survival curves
+
+
+
+
+
+
+
+miRNAcurves <- survfit(Surv(Days, Survival) ~ Mutant, data=DCV_noPBS)
+ggsurvplot(miRNAcurves,
+           data = DCV_noPBS,               #change to the virus under analysis
+           axes.offset = FALSE,            #changes axes to start at zero
+           line = c(1,1),
+           pval = 0.19,             #change to coxme pvalue
+           xlab = "Time in days",
+           ylab = "Proportional survival",
+           break.time.by = 1, 
+           break.y.by = 0.2,
+           ggtheme = theme_classic(),
+           legend.title = "Genotype",      #change legend title to suit your analysis
+           legend.labs = 
+             c("w1118", "miR- KO")         #change the legends to the miRNA
+)
+
+# Save survival plot ----
+ggsave(here("plots", "example.png"))
+
 
